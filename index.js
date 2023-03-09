9edc68254180eb809eaa1b55c767c84c9b647f60
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-09 13:08:01 -0800

    practice

---
+++ index.js
@@ -0,0 +1,9 @@
+let      str="Brown fox";
+for (let elem of str) {
+    if(elem===elem.toUppercase()){
+        console.log("b")
+         }
+    if(elem===elem.toLowercase()){
+        console.log("r")
+       }
+}
